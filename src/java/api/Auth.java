39cabaaaab4fd070b5dcc9c516e3ab11047beb67
Yahir Trejo <yahardy58@gmail.com>
2018-09-20 21:57:25 -0500

    feat: first commit

---
+++ src/java/api/Auth.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import libraries.Utils;
+
+/**
+ *
+ * @author Yahir
+ */
+public class Auth extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        System.out.println("Service called");
+        try (PrintWriter out = response.getWriter();){
+            String user = Utils.getBody(request);
+            System.out.println("user " + user);
+            Enumeration e = request.getParameterNames();
+            while(e.hasMoreElements()) {
+                System.out.println("header " + e.nextElement());
+            }
+            out.print(user);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
---
+++ src/java/libraries/Utils.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libraries;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Yahir
+ */
+public class Utils {
+    
+    public static String getBody(HttpServletRequest request) throws IOException {
+
+    StringBuilder buffer = new StringBuilder();
+    BufferedReader reader = request.getReader();
+    String line;
+    while ((line = reader.readLine()) != null) {
+        buffer.append(line);
+    }
+    String data = buffer.toString();
+    
+    return data;
+}
+    
+}
